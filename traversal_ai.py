8f59df5bc18169029830aef810a85f08a342a6c4
BPitts8019 <bradleyj.pitts@gmail.com>
2020-08-31 21:43:27 -0700

    feat: Chooses a random unexplored location

--- traversal_ai.py
+++ traversal_ai.py
@@ -1,29 +1,52 @@
 from queue import Queue, LifoQueue
 from world import World
 from room import Room
+import random
 
 
-def isRoomVisited():
-    pass
+def getUnexploredExits(cur_room, visited_rooms):
+    unexplored_exits = []
+    for direction in cur_room.get_exits():
+        if direction not in visited_rooms[cur_room.id]:
+            unexplored_exits.append(direction)
+
+    return unexplored_exits
 
 
 def buildTraversalPath(world: World):
     traversal_stack = LifoQueue()
-    visited_rooms = set()
+    visited_rooms = {}
+    opp_direction = {
+        "n": "s",
+        "e": "w",
+        "s": "n",
+        "w": "e"
+    }
 
-    traversal_stack.put((world.starting_room, []))
+    traversal_stack.put((world.starting_room, -1, []))
     while not traversal_stack.empty():
-        cur_room, room_path = traversal_stack.get()
+        cur_room, prev_room_id, room_path = traversal_stack.get()
         exits = cur_room.get_exits()
         if cur_room.id not in visited_rooms:
             print(cur_room)
-            visited_rooms.add(cur_room.id)
-            for direction in exits:
-                traversal_stack.put((cur_room.get_room_in_direction(
-                    direction), room_path + [direction]))
+            # Mark room as visited
+            visited_rooms[cur_room.id] = set()
 
-            if len(exits) == 1 and cur_room.get_room_in_direction(exits[0]).id in visited_rooms:
+            # Mark directions moved as explored
+            if len(room_path) > 0:
+                visited_rooms[prev_room_id].add(room_path[-1])
+                visited_rooms[cur_room.id].add(opp_direction[room_path[-1]])
+
+            if len(exits) == 1 and prev_room_id in visited_rooms:
+                # reached a dead end; go back to a room with unexpolored exits
                 return room_path
+            else:
+                # choose a random unexplored direction
+                direction = random.choice(
+                    getUnexploredExits(cur_room, visited_rooms))
+                traversal_stack.put((cur_room.get_room_in_direction(
+                    direction), cur_room.id, room_path + [direction]))
+
     # isDeadEnd = False
     # rtn_path = []
     # traversal_stack = LifoQueue()
